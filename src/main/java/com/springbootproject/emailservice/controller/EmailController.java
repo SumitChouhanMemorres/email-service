f5fce0c00ac1a92f0eae1c5317da0a5486f5663a
Sumit Chauhan <sumit.chouhan@memorres.com>
2023-08-29 10:31:04 +0530

    Initial Commit

---
+++ src/main/java/com/springbootproject/emailservice/controller/EmailController.java
@@ -0,0 +1,19 @@
+package com.springbootproject.emailservice.controller;
+
+import com.springbootproject.emailservice.model.EmailDetails;
+import com.springbootproject.emailservice.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("sendMail")
+    public String sendMail(@RequestBody EmailDetails details){
+        return emailService.sendSimpleMail(details);
+    }
+}
